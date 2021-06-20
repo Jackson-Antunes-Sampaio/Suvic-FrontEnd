f66eaab0cd786c30c839bd3c03ae80d79c9dd3e3
Vluameiro <valdimiro.luameiro@hotmail.com>
2021-06-20 12:11:21 +0100

    main

--- lib/controllers/agendament_controller.dart
+++ lib/controllers/agendament_controller.dart
@@ -5,6 +5,7 @@ import 'package:get/get.dart';
 class AgendamentController extends GetxController {
   var repository = AgendamentRepository();
   List agendaments = [].obs;
+  var loading = false.obs;
 
   static AgendamentController get to => Get.find();
 
@@ -13,7 +14,11 @@ class AgendamentController extends GetxController {
   }
 
   insert(AgendamentModel agendament) async {
+    loading.value = true;
     await repository.insert(agendament);
+    loading.value = false;
+    Get.snackbar('Sucesso', 'O seu agendamento foi efectuado com sucesso',
+        snackPosition: SnackPosition.BOTTOM);
     agendaments.add(agendament);
   }
 }
