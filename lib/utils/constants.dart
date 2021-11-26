51da20bd25a1e177412500e33920bb51698394bf
MAPJBR <66679202+MAPJBR@users.noreply.github.com>
2021-11-26 20:50:40 -0300

    Modificação da URL para staging

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,6 @@
 
 //const API_URL = "http://3.140.194.155:3000/";
 
-const API_URL = "https://ec2-18-231-166-223.sa-east-1.compute.amazonaws.com:8443/";
+const API_URL = 'https://staging.suvic.com.br/';
 
 const API_URL_PAGARME = "https://api.pagar.me/1/";
